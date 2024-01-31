a9b29a8f7479147ec5c4c1487b844bc21c3b641c
Nicolas Ferrand <nicolas.ferrand38@gmail.com>
2024-01-31 09:59:57 +0100

    fixed timezone issue

--- public/js/SeasonButtonGenerator.js
+++ public/js/SeasonButtonGenerator.js
@@ -14,11 +14,12 @@ class SeasonButtonGenerator
         {
             i++;
             let a = document.createElement("a");
-            let sdate = datas.starting_date.split('T');
-            a.text = sdate[0];
-            a.setAttribute("href","./season.html?starting_date=" + sdate[0] + "&id=" + i);
-            a.setAttribute("class","linkToSeasonPage");
-            a.setAttribute("id","linkToSeasonPage" + i);
+            let sdate = new Date(datas.starting_date);
+            let formattedDate = sdate.getFullYear() + "-" + ('0' + (sdate.getMonth() + 1)).slice(-2) + "-" + ('0' + sdate.getDate()).slice(-2);
+            a.text = formattedDate;
+            a.setAttribute("href", "./season.html?starting_date=" + formattedDate + "&id=" + i);
+            a.setAttribute("class", "linkToSeasonPage");
+            a.setAttribute("id", "linkToSeasonPage" + i);
             this.DivToFill.appendChild(a);
         }
     }
